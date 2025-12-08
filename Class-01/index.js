df05901c8090120653b78a5dd7b93cfcac97070a
yash <yaxj28@gmail.com>
2025-12-08 19:22:11 +0530

    HashingInputs

---
+++ Class-01/index.js
@@ -0,0 +1,41 @@
+import crypto from "crypto";
+
+const input = "100xdevs";
+
+const hash = crypto.createHash('sha256').update(input).digest('hex');
+
+console.log(hash);
+
+//Code for finding hash starting with 00000 
+function bruteforce(prefix){ 
+    let input = 0;
+    while (true){
+        let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if (hash.startsWith(prefix)){
+            return {
+                input : inputStr,
+                hash : hash
+            }
+        };
+        input++;
+    }
+};
+
+
+// Code for input string to start with the particular thing
+// like 100xdevs + hello
+function str(string){
+    let input = 0;
+    while (true){
+        let inputStr = '100xdevs' + string;
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        return hash;
+    }
+}
+
+
+const result = bruteforce('000000000000');
+console.log(result);
+const result2 = str('2274885');
+console.log(result2);
