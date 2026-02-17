f4138a82c93171565eee90101ed98583a0deaf03
yash <yaxj28@gmail.com>
2026-02-17 19:40:29 +0530

    incement_counter_test

--- Native_rust_counter_program/counter_pinoccio/src/lib.rs
+++ Native_rust_counter_program/counter_pinoccio/src/lib.rs
@@ -1,35 +1,55 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use pinocchio::{
-    AccountView, Address, ProgramResult, entrypoint, error::ProgramError, sysvars::slot_hashes::log
+    AccountView, Address, ProgramResult, entrypoint, error::ProgramError, sysvars::slot_hashes::log,
 };
 use pinocchio_system::instructions::CreateAccount;
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Counter {
+    count: u32,
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub enum CounterProgram {
+    Initialize,
+    Increment,
+}
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    program_id : &Address,
+    program_id: &Address,
     accounts: &[AccountView],
-    _instruction_data : &[u8]
-)-> ProgramResult {
-    let [from, to, system_program] = accounts else
-        {
-        return Err(ProgramError::NotEnoughAccountKeys);
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instr = CounterProgram::try_from_slice(instruction_data)
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
+
+    let mut counter_account = &accounts[0];
+
+    let owner = unsafe {
+        counter_account.owner()
     };
-    
+    if owner != program_id {
+        return Err(ProgramError::IncorrectProgramId);
+    };
 
-    let balance = from.lamports();
-    
-    CreateAccount{
-        from : from,
-        to : to,
-        lamports : 1_000_000_000,
-        space : 10,
-        owner : program_id
-    }.invoke()
+    match instr{
+        CounterProgram::Initialize => {
+            let count = Counter{
+                count : 0
+            };
 
-    // core::hint::black_box(Ok(()))
+        },
+        CounterProgram::Increment => {
+
+        }
+    }
+
+  
+
+    Ok(())
 }
 
-
- 
 //compute units in a way is no. of instructions your program gonna execute.
 //Anchor does like includes a lots of copies. That's why a cu is higher.
--- Native_rust_counter_program/counter_rust/test/index.test.ts
+++ Native_rust_counter_program/counter_rust/test/index.test.ts
@@ -1,12 +1,12 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, VersionedTransaction } from "@solana/web3.js";
-import {expect, test} from "bun:test";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, TransactionInstruction, VersionedTransaction } from "@solana/web3.js";
+import { expect, test } from "bun:test";
 import { counter_size, schema } from "./types";
 import * as borsh from 'borsh';
 
-let counterAccountKeypair : Keypair;
-let adminKeypair : Keypair;
+let counterAccountKeypair: Keypair;
+let adminKeypair: Keypair;
 
-test("Account Initialized", async ()=>{
+test("Account Initialized", async () => {
     adminKeypair = Keypair.generate();
     counterAccountKeypair = new Keypair();
 
@@ -16,32 +16,98 @@ test("Account Initialized", async ()=>{
     // console.log("Airdrop done");
 
     const data = await connection.getAccountInfo(adminKeypair.publicKey);
-    console.log("data is : ",data);
+    console.log("data is : ", data);
 
     const programId = new PublicKey("HFKwi65ibRDVScWFyWM2yKvhj8Na2cGw4TyiLUWk4Hee");
 
     const lamports = await connection.getMinimumBalanceForRentExemption(counter_size);
 
     const createTransInst = SystemProgram.createAccount({
-        fromPubkey : adminKeypair.publicKey,
-        lamports : lamports,
-        newAccountPubkey : counterAccountKeypair.publicKey,
-        programId : programId,
-        space : counter_size
+        fromPubkey: adminKeypair.publicKey,
+        lamports: lamports,
+        newAccountPubkey: counterAccountKeypair.publicKey,
+        programId: programId,
+        space: counter_size
     });
     let tx = new Transaction();
     tx.add(createTransInst)
     await sendAndConfirmTransaction(connection, tx, [adminKeypair, counterAccountKeypair,]);
-    console.log("Pub Key of data account : ",counterAccountKeypair.publicKey.toBase58());
+    console.log("Pub Key of data account : ", counterAccountKeypair.publicKey.toBase58());
 
     const counterAccount = await connection.getAccountInfo(counterAccountKeypair.publicKey);
     console.log("Data Account Info : ", counterAccount);
-    if (!counterAccount){
+    if (!counterAccount) {
         throw new Error("Account not found");
     }
 
     const counter = borsh.deserialize(schema, counterAccount?.data);
     console.log("Counter is : ", counter);
+    //@ts-ignore
     console.log(counter?.count);
+    //@ts-ignore
     expect(counter.count).toBe(0);
 });
+
+test("multiple Increment", async () => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const programId = new PublicKey("HFKwi65ibRDVScWFyWM2yKvhj8Na2cGw4TyiLUWk4Hee");
+
+    for (let i = 0; i < 5; i++) {
+        const incrementIx = new TransactionInstruction({
+            programId: programId,
+            keys: [
+                {
+                    pubkey: counterAccountKeypair.publicKey,
+                    isWritable: true,
+                    isSigner: true
+                }
+            ],
+            data: Buffer.from([1])
+        });
+
+        const tx = new Transaction().add(incrementIx);
+        await sendAndConfirmTransaction(connection, tx, [adminKeypair]);
+    }
+
+    const counterAccount = await connection.getAccountInfo(counterAccountKeypair.publicKey);
+    if (!counterAccount) throw new Error("Account not found");
+
+    const counter = borsh.deserialize(schema, counterAccount.data);
+    console.log(
+        "Counter is : ", counter
+    );
+    //@ts-ignore
+    expect(counter.count).toBe(6);
+});
+
+
+test("counter increment", async () => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const programId = new PublicKey("HFKwi65ibRDVScWFyWM2yKvhj8Na2cGw4TyiLUWk4Hee");
+
+    //Build increment instruction data
+    const incrementIx = new TransactionInstruction({
+        programId: programId,
+        keys: [
+            {
+                pubkey: counterAccountKeypair.publicKey,
+                isWritable: true,
+                isSigner: true
+            }
+        ],
+        data: Buffer.from([1])
+    });
+
+    const tx = new Transaction().add(incrementIx);
+    await sendAndConfirmTransaction(connection, tx, [adminKeypair]);
+
+    const counterAccount = await connection.getAccountInfo(counterAccountKeypair.publicKey);
+    if (!counterAccount) throw new Error("Account not found");
+
+    const counter = borsh.deserialize(schema, counterAccount.data);
+    console.log(
+        "Counter is : ", counter
+    );
+    //@ts-ignore
+    expect(counter.count).toBe(1);
+})
