b61545efd9b4c11d53e029fc016836e4562d3a1a
yash <yaxj28@gmail.com>
2026-03-31 07:34:16 +0530

    inovke_returned

--- return_result_pinocchio/src/lib.rs
+++ return_result_pinocchio/src/lib.rs
@@ -1,8 +1,12 @@
 
 use pinocchio::{
-  AccountView, Address, ProgramResult, cpi::set_return_data, entrypoint
+  AccountView, Address, ProgramResult, entrypoint, sysvars::instructions::Instructions
 };
+use solana_cpi::{get_return_data, invoke, set_return_data};
 use solana_program_log::log;
+use solana_instruction::Instruction;
+
+mod normal_return;
 
 entrypoint!(process_instruction);
 
@@ -11,9 +15,14 @@ pub fn process_instruction(
   accounts: &[AccountView],
   instruction_data: &[u8],
 ) -> ProgramResult {
-  log!("Hello from my pinocchio program!");
-  //Return success. 
-  let data = "yashIsaChampion".as_bytes();
-  set_return_data(data);
+  let program_to_call = &accounts[0];
+  let instru = Instruction{
+    program_id : *program_to_call.address(),
+    accounts : vec![],
+    data : vec![]
+  };
+  invoke(&instru, &[])?;
+  let data = get_return_data().unwrap();
+  
   Ok(())
 }
